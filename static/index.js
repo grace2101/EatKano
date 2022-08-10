4d466fac3c31e8569da644205523a7a8a0365a8d
grace2101 <110990661+grace2101@users.noreply.github.com>
2022-08-11 01:31:41 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -504,8 +504,8 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
 
         if (cps <= 3) return I18N['text-level-1'];
         if (cps <= 5) return I18N['text-level-2'];
-        if (cps <= 8)  return I18N['text-level-3'];
-        if (cps <= 12) return I18N['text-level-4'];
+        if (cps <= 7)  return I18N['text-level-3'];
+        if (cps <= 10) return I18N['text-level-4'];
         return I18N['text-level-5'];
     }
 
